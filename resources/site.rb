6e76dcbf99c785f64154b6e1a6834ca30385ee64
Râu Cao <raucao@kip.pe>
2023-07-30 12:33:04 +0200

    Migrate openresty site to new-style resource

--- resources/site.rb
+++ resources/site.rb
@@ -23,9 +23,73 @@ actions :enable, :disable
 
 default_action :enable
 
-attribute :name, kind_of: String, name_attribute: true
-attribute :template, kind_of: String, default: nil
-attribute :variables, kind_of: Hash, default: {}
-attribute :cookbook, kind_of: String, default: nil
-attribute :timing, [:delayed, :immediately], default: :delayed
-attribute :redirect_http, kind_of: [TrueClass, FalseClass], default: true
+property :name, kind_of: String, name_property: true
+property :template, kind_of: String, default: nil
+property :variables, kind_of: Hash, default: {}
+property :cookbook, kind_of: String, default: nil
+property :timing, [:delayed, :immediately], default: :delayed
+property :redirect_http, kind_of: [TrueClass, FalseClass], default: true
+
+action :enable do
+  site_name = (new_resource.name == 'default') ? '000-default' : new_resource.name
+  openresty_dir = node['openresty']['dir']
+
+  if new_resource.template
+    if new_resource.redirect_http
+      template "#{openresty_dir}/sites-available/#{site_name}_http" do
+        source "redirect_http.conf.erb"
+        owner 'root'
+        group 'root'
+        mode 00644
+        variables new_resource.variables
+        cookbook 'openresty'
+        if node['openresty']['service']['start_on_boot']
+          notifies :reload, node['openresty']['service']['resource'], new_resource.timing
+        end
+      end
+
+      link "#{openresty_dir}/sites-enabled/#{site_name}_http" do
+        to "#{openresty_dir}/sites-available/#{site_name}_http"
+        action :create
+        notifies :reload, node['openresty']['service']['resource'], new_resource.timing
+      end
+    end
+
+    template "#{openresty_dir}/sites-available/#{site_name}" do
+      source new_resource.template
+      owner 'root'
+      group 'root'
+      mode 00644
+      variables new_resource.variables
+      cookbook new_resource.cookbook
+      if node['openresty']['service']['start_on_boot']
+        notifies :reload, node['openresty']['service']['resource'], new_resource.timing
+      end
+    end
+  end
+
+  link "#{openresty_dir}/sites-enabled/#{site_name}" do
+    to "#{openresty_dir}/sites-available/#{site_name}"
+    action :create
+    notifies :reload, node['openresty']['service']['resource'], new_resource.timing
+  end
+end
+
+action :disable do
+  site_name = (new_resource.name == 'default') ? '000-default' : new_resource.name
+  openresty_dir = node['openresty']['dir']
+
+  if new_resource.redirect_http
+    link "#{openresty_dir}/sites-enabled/#{site_name}_http" do
+      to "#{openresty_dir}/sites-available/#{site_name}_http"
+      action :delete
+      notifies :reload, node['openresty']['service']['resource'], new_resource.timing
+    end
+  end
+
+  link "#{openresty_dir}/sites-enabled/#{site_name}" do
+    to "#{openresty_dir}/sites-available/#{site_name}"
+    action :delete
+    notifies :reload, node['openresty']['service']['resource'], new_resource.timing
+  end
+end
